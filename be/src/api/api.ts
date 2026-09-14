9c12f927b8fd5fa1406a646eec2796b365b10ce0
Maksym Nikitchenko <m.nikitchenko@softonix.org>
2026-09-14 19:33:36 +0300

    minor fix

--- be/src/api/api.ts
+++ be/src/api/api.ts
@@ -2,7 +2,7 @@ import { buildServer } from 'src/api/build-server';
 
 buildServer().then((server) => {
 	return server.listen({
-		port: Number(server.env.PORT ?? 1506),
+		port: Number(server.env.PORT ?? 1606),
 		host: server.env.HOST ?? '127.0.0.1'
 	});
 });
